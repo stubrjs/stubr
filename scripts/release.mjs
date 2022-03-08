a35ee9bb3a0fbb21220600a130df8b8b47a9ead3
Christian Hartkemeyer <christian@MBPvonChristian.fritz.box>
2022-03-08 07:36:03 +0100

    feat: extension of release script

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -1,5 +1,7 @@
 import { execa } from 'execa';
+import chalk from 'chalk';
 
+const isDryRun = process.env.DRY_RUN === 'true';
 const versionType = process.env.VERSION_TYPE;
 const releaseType = process.env.RELEASE_TYPE;
 
@@ -19,27 +21,48 @@ if (!releaseType) {
 
 const run = (bin, args, opts = {}) =>
     execa(bin, args, { stdio: 'inherit', ...opts });
+const dryRun = (bin, args, opts = {}) =>
+    console.log(chalk.blue(`[dryrun] ${bin} ${args.join(' ')}`), opts);
+const runIfNotDry = isDryRun ? dryRun : run;
+const step = (msg) => console.log(chalk.cyan(`\n${msg}`));
 
 async function main() {
-    await run('yarn', ['stubr:test']);
-    await run('yarn', ['build']);
+    step('performing unit tests...');
+    await runIfNotDry('yarn', ['stubr:test']);
 
+    step('performing builds as sanity check...');
+    await runIfNotDry('yarn', ['build']);
+
+    step('bumping version...');
     if (releaseType === 'preview') {
         await run('npx', [
             'lerna',
             'version',
             '--preid',
             'beta',
-            `pre${versionType}`
+            `pre${versionType}`,
+            '--yes',
         ]);
     } else if (releaseType === 'release') {
-        await run('npx', ['lerna', 'version', versionType]);
+        await run('npx', ['lerna', 'version', versionType, '--yes']);
     } else {
         console.error(`unkown releaseType "${releaseType}"`);
         process.exit(1);
     }
+
+    step('generating release notes...');
+    await run('yarn', ['changelog']);
+
+    step('updating lock files...');
+    await run('yarn', ['install']);
+
+    step('performing new builds...');
+    await run('yarn', ['build']);
+
+    step('\nPushing to GitHub...');
+    await runIfNotDry('git', ['push']);
 }
 
-main().catch(err => {
+main().catch((err) => {
     console.error(err);
 });
